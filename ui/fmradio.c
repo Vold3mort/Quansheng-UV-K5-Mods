b46c9bfc58756237e4d25386fa30eb7023a861e0
Nunu <kamilsos@gmail.com>
2024-01-12 15:51:55 +0100

    add cp claims.

--- ui/fmradio.c
+++ ui/fmradio.c
@@ -1,6 +1,9 @@
 /* Copyright 2023 Dual Tachyon
  * https://github.com/DualTachyon
  *
+ * Copyright 2024 kamilsss655
+ * https://github.com/kamilsss655
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
