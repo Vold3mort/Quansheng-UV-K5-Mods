a8259c05ec342b6ed0ca6ca3c6d3c43993a54e9e
Nunu <kamilsos@gmail.com>
2024-01-13 17:18:59 +0100

    update factory reset

--- board.c
+++ board.c
@@ -856,6 +856,8 @@ void BOARD_FactoryReset(bool bIsAll)
 	{
 		RADIO_InitInfo(gRxVfo, FREQ_CHANNEL_FIRST + BAND6_400MHz, 43350000);
 		gEeprom.RX_OFFSET = 0;
+		gEeprom.POWER_ON_PASSWORD = PASSWORD_OFF;
+		SETTINGS_SaveSettings();
 		// set the first few memory channels
 		for (i = 0; i < ARRAY_SIZE(gDefaultFrequencyTable); i++)
 		{
